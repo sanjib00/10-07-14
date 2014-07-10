a26e81055772d57f5a26bf18032b00b5ec7aedea
towfiqpiash <towfiqpiash@gmail.com>
2014-07-10 12:44:21 +0600

    define and accessing user name

--- student.php
+++ student.php
@@ -2,13 +2,18 @@
 
 //phpinfo();
 class User{              //declaring a object
-var $name = "FTFL";
-var $email = "";
+	var $name = "FTFL";
+	var $email = "";
+		function getName(){
+			return "FTFL2";
+
+}
 }
 
 $user = new User();      //instanttiate the object from the class
 //var_dump($user);
 echo $user->name;
 
+echo $user->getName();
 
 ?>
