39a62bb87beb72537eaf80d006d67084a97615db
towfiqpiash <towfiqpiash@gmail.com>
2014-07-10 14:43:11 +0600

    uuuuuuuuuuuuu

---
+++ oop.php
@@ -0,0 +1,22 @@
+<?php
+
+		class User{
+		
+			private function userAge(){
+			
+			return 'some value';
+			}
+			
+		}
+		
+		class Person extends User{
+		public function getUserage(){
+		return $this->userAge();
+	}
+	
+	}
+	$person = new Person();
+	echo $person->getUserAge();
+	echo $person->userAge();
+
+?>
--- student.php
+++ student.php
@@ -4,16 +4,34 @@
 class User{              //declaring a object
 	var $name = "FTFL";
 	var $email = "";
+	
+	function __construct($name="FTFL", $email="lict@ftfl.com"){
+	
+		$this->name = $name;
+		$this->email = $email;
+		}
 		function getName(){
-			return "FTFL2";
+			return $this->name;
 
 }
-}
+		function getEmail(){
+			return $this->email;
 
-$user = new User();      //instanttiate the object from the class
+}
+		
+
+
+//$user = new User();      //instantiate the object from the class
+$user1 = new User();
+$user2 = new User();
+
 //var_dump($user);
-echo $user->name;
+//echo $user->name;
+$user->name= 'TestFTFL';
+$user1->name= 'oop';
 
 echo $user->getName();
+echo $user1->getName();
 
+echo $user1::
 ?>
